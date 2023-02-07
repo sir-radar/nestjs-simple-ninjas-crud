85e6a7ea3f3f3b601112352176c74a32f3ea54d3
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-07 13:08:22 +0100

    added user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjaModule } from './ninja/ninja.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [NinjaModule],
+  imports: [NinjaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
