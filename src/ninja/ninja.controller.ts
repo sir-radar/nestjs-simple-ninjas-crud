233af2cd227a4e91a2b28cfb8b207c70e8103496
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-13 04:26:59 +0100

    set up ninja controller

--- src/ninja/ninja.controller.ts
+++ src/ninja/ninja.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninja')
-export class NinjaController {}
+export class NinjaController {
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  @Get(':id')
+  getNinja(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return { name: createNinjaDto.name };
+  }
+
+  @Put(':id')
+  updateNinja(@Param() id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return { id, name: updateNinjaDto.name };
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return { id };
+  }
+}
